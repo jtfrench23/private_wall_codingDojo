b0ab6704f4dde400c032a5332398fd8729d8b2f3
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-02-12 12:13:20 -0600

    updated style on dashboard

--- flask_app/controllers/messages.py
+++ flask_app/controllers/messages.py
@@ -5,7 +5,19 @@ from flask import render_template, redirect, request, session, flash
 from flask_app.models import user
 from flask_app.models import message
 
+
+# CREATE CONTROLLERS
 @app.route("/create_message", methods=["POST"])
 def new_message():
     message.Message.create_message(request.form)
     return redirect("/dashboard")
+
+
+#READ CONTROLLERS
+
+#UPDATE CONTROLLERS
+
+#DELETE CONTROLLERS
+@app.route("/delete/<int:id>")
+def delete_message(id):
+    pass
